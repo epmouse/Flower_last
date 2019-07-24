209d808c385b7f2fcf0836256e46d7cd195aa2f5
liuxing <18838265776@163.com>
2019-07-24 18:15:40 +0800

    拍照界面添加内框

--- lib/pages/page_camera.dart
+++ lib/pages/page_camera.dart
@@ -3,7 +3,6 @@ import 'package:flower_last/utils/util_camera.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
-
 class CameraPage extends StatefulWidget {
   @override
   _CameraPageState createState() {
@@ -17,7 +16,8 @@ class _CameraPageState extends State<CameraPage> {
   @override
   void initState() {
     super.initState();
-    controller = CameraController(CameraUtils.cameras[0], ResolutionPreset.medium);
+    controller =
+        CameraController(CameraUtils.cameras[0], ResolutionPreset.medium);
     controller.initialize().then((_) {
       if (!mounted) {
         return;
@@ -38,8 +38,22 @@ class _CameraPageState extends State<CameraPage> {
       return Container();
     }
     return AspectRatio(
-        aspectRatio:
-        controller.value.aspectRatio,
-        child: CameraPreview(controller));
+      aspectRatio: controller.value.aspectRatio,
+      child: Stack(
+        children: <Widget>[CameraPreview(controller), Center(
+          child: Container(
+            height: 250,
+            width: 250,
+            decoration: BoxDecoration(
+            color: Colors.transparent,
+             border: Border.all(
+               color: Colors.amber,
+               width: 0.3,
+             ),
+            ),
+          ),
+        ), ],
+      ),
+    );
   }
 }
